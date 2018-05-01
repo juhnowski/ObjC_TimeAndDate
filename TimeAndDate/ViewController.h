feecf5a75e0e92fa0a5997f4b495a84bcaf25073
Juhnowski <juhnowski@gmail.com>
2018-05-01 18:01:29 +0300

    initial

--- TimeAndDate/ViewController.h
+++ TimeAndDate/ViewController.h
@@ -8,8 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+    NSTimer *timer;
+}
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
 
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
 
 @end
 
--- TimeAndDate/ViewController.m
+++ TimeAndDate/ViewController.m
@@ -17,6 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(updateTimer) userInfo:nil repeats:YES];
+}
+
+-(void)updateTimer {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"hh:mm:ss"];
+    self.timeLabel.text = [formatter stringFromDate:[NSDate date]];
+    
+    NSDateFormatter *formatter2 = [[NSDateFormatter alloc] init];
+    [formatter2 setDateFormat:@"YYYY-MM-dd"];
+    self.dateLabel.text = [formatter2 stringFromDate:[NSDate date]];
 }
 
 
